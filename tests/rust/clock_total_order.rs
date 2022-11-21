e5dd80d776e3b2c7c187b01063128ca656fe0ed6
Ryan Newton <newton@meta.com>
2022-11-21 02:36:11 -0800

    a regression test regarding clock total order across threads
    Summary:
    If we have a causal synchronization from thread1 to thread2, we would expect clock measurements that are ordered (by that "happens before" relation) to also be ordered.
    
    Hermit already satisfies this condition.  Because even if thread-local time lags behind global (vector clock) time, whenever we service the clock_gettime syscall, we use the global time, so it is nice and monotonic.
    
    But because I'm changing things with how local/global time relate, I want this test there as regression protection.
    
    Note: this isn't working under trace-replay mode yet, because of a divergence in an extra register-prehook (plus a downstream difference in a sleep(0) vs TraceReplay COMMIT).
    
    Reviewed By: VladimirMakaev
    
    Differential Revision: D40590539
    
    fbshipit-source-id: d710cb2c7fd2b65fb57babf56d8f81014314a375

---
+++ tests/rust/clock_total_order.rs
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! Look at total order of clock_gettime across threads
+
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::Ordering;
+use std::sync::Arc;
+use std::thread;
+
+// use nix::sys::time::TimeSpec;
+use nix::sys::time::TimeValLike;
+// use nix::time::clock_getres;
+use nix::time::clock_gettime;
+// use nix::time::clock_settime;
+use nix::time::ClockId;
+
+/// Absolute nanoseconds.
+type Tm = u128;
+
+/// Return absolute time in nanoseconds.
+fn now() -> Tm {
+    let clockid = ClockId::CLOCK_MONOTONIC;
+    let tmspc = clock_gettime(clockid).unwrap();
+    let secs = tmspc.num_seconds();
+    // num_nanoseconds actually seems to report absolute time, not just the ns
+    // component. There's no obvious way to get just the ns component out of the timespec:
+    let ns = tmspc.num_nanoseconds() % 1_000_000_000;
+    (secs as Tm) * 1_000_000_000 + (ns as Tm)
+}
+
+#[inline(never)]
+fn do_work(iters: u64) -> f64 {
+    let mut spurious = 1.1;
+    for _ in 0..iters {
+        spurious /= 0.999999999999;
+        spurious += 0.000001;
+    }
+    spurious
+}
+
+fn thread1(flag: Arc<AtomicU64>) -> (Tm, Tm) {
+    let tm1 = now();
+    do_work(10_000);
+    let tm2 = now();
+    flag.store(1, Ordering::SeqCst);
+    (tm1, tm2)
+}
+
+fn thread2(flag: Arc<AtomicU64>) -> (Tm, Tm, Tm) {
+    let tm3 = now();
+    while flag.load(Ordering::SeqCst) == 0 {
+        // wait.
+    }
+    let tm4 = now();
+    do_work(10_000);
+    let tm5 = now();
+    (tm3, tm4, tm5)
+}
+
+fn main() {
+    let flag = Arc::new(AtomicU64::new(0));
+    let flag2 = flag.clone();
+    let h2 = thread::spawn(move || thread2(flag2));
+    let h1 = thread::spawn(move || thread1(flag));
+
+    let (tm1, tm2) = h1.join().unwrap();
+    let (tm3, tm4, tm5) = h2.join().unwrap();
+
+    println!("First time, anchor: {}", tm1);
+    println!("2nd time, relative {}", (tm2 as i128) - (tm1 as i128));
+    println!("3nd time, relative {}", (tm3 as i128) - (tm1 as i128));
+    println!("4th time, relative {}", (tm4 as i128) - (tm1 as i128));
+    println!("5th time, relative {}", (tm5 as i128) - (tm1 as i128));
+
+    assert!(tm1 < tm2);
+    assert!(tm3 <= tm4);
+    assert!(tm4 <= tm5);
+
+    // Totally ordered ACROSS threads:
+    assert!(
+        tm2 <= tm4,
+        "clock values were not ordered across threads, in spite of causal path"
+    );
+}
