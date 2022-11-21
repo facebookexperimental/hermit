f64af909dc98735c96c870c2ed6c44e1a9db7bd9
Ryan Newton <newton@meta.com>
2022-11-21 02:36:11 -0800

    add racewrite_nostdlib analyze test
    Summary:
    This is a very-near-minimal test that performs only 4 syscalls (6 including exits). That makes it easier to make specific assertions about the expected behavior.
    
    Tweak the testing script so that it passes extra arguments to analyze and expects a particular string to be in its output.
    
    Reviewed By: VladimirMakaev
    
    Differential Revision: D41423972
    
    fbshipit-source-id: cfd7ccce3058d7c8963f0163ce2df0e91af9d444

--- tests/util/hermit_analyze_test.sh
+++ tests/util/hermit_analyze_test.sh
@@ -29,24 +29,32 @@ if [[ "$KEEP_LOGS" != "0" ]]; then
     set -x
 fi
 
-# Common arguments:
-if [[ -z "$HERMIT_ARGS" ]]; then
-    # hermit analyze args:
-    HERMIT_ARGS="--analyze-seed=0 "
-    if [[ "$DO_MINIMIZE" != "0" ]]; then
-        HERMIT_ARGS+="--minimize "
-    fi
-    HERMIT_ARGS+="--search "
-    if [[ "$KEEP_LOGS" != "0" ]]; then
-        HERMIT_ARGS+="--verbose "
-    fi
-    HERMIT_ARGS+=" -- "
-    # hermit run args:
-    HERMIT_ARGS+="--base-env=minimal "
-    HERMIT_ARGS+="--chaos "
-    HERMIT_ARGS+="--preemption-timeout=400000 "
+# Additional arguments to analyze:
+if [[ -z "$ANALYZE_OPTS" ]]; then
+    ANALYZE_OPTS=""
 fi
+
+if [[ -z "$EXPECTED_OUTPUT" ]]; then
+    EXPECTED_OUTPUT=""
+fi
+
 set -eu
+
+# hermit analyze args:
+HERMIT_ARGS="--analyze-seed=0 "
+if [[ "$DO_MINIMIZE" != "0" ]]; then
+    HERMIT_ARGS+="--minimize "
+fi
+HERMIT_ARGS+="--search "
+if [[ "$KEEP_LOGS" != "0" ]]; then
+    HERMIT_ARGS+="--verbose "
+fi
+HERMIT_ARGS+=" -- "
+# hermit run args:
+HERMIT_ARGS+="--base-env=minimal "
+HERMIT_ARGS+="--chaos "
+HERMIT_ARGS+="--preemption-timeout=400000 "
+
 TEMP=$(mktemp -d /tmp/analyze_test_XXXXX)
 echo ":: [analyze_test] Temporary workspace: $TEMP"
 TEMPLOG="${TEMP}/log.txt"
@@ -63,11 +71,16 @@ function cleanup {
 echo ":: [analyze_test] Invoking analyze with: $HERMIT analyze $HERMIT_ARGS -- $TESTBIN"
 
 # shellcheck disable=SC2086 # Intended splitting of args and command:
-$HERMIT analyze $HERMIT_ARGS -- "$TESTBIN" > >(tee "$TEMPLOG") \
+$HERMIT analyze $ANALYZE_OPTS $HERMIT_ARGS -- "$TESTBIN" > >(tee "$TEMPLOG") \
   || (echo "Analyze failed."; exit 1)
 
 echo ":: [analyze_test] Searching for printed backtraces in $TEMPLOG"
 grep -q "Guest .* below backtrace" "$TEMPLOG" \
    || (echo "Stack trace not printed by hermit analyze!"; exit 1)
-code=$?
-exit $code
+
+if [[ "$EXPECTED_OUTPUT" != "" ]]; then
+    grep -q "$EXPECTED_OUTPUT" "$TEMPLOG" \
+    || (echo "Expected this string in hermit analyze output, but it was missing: '$EXPECTED_OUTPUT'"; exit 1)
+fi
+
+echo ":: [analyze_test] All checks matched, test passed."
