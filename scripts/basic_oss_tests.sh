7e2f9da026380230dfd17f2ede686317e1733d8c
Ryan Rhodes Newton <newton@fb.com>
2022-11-28 08:11:16 -0800

    [hermit][open-source] Turn off RCBs for github actions tests
    Summary:
    The VMs running github actions do not expose perf counters, as seen here:
    
      https://github.com/facebookexperimental/hermit/actions/runs/3560943420/jobs/5981398423
    
       WARNING: --preemption-timout requires hardware perf counters which is not supported on this host, resetting preemption-timeout to 0
    
    Therefore, we run only a limited subset of tests under hermit run *without* RCB-based preemption of threads (i.e. busy-wait breaking).
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- scripts/basic_oss_tests.sh
+++ scripts/basic_oss_tests.sh
@@ -10,22 +10,58 @@
 
 set -eEuo pipefail
 
+cd "$(dirname $0)"
+cd "../"
+
 rootdir="$(pwd)"
 hermit="$rootdir/target/debug/hermit"
 hverify="$rootdir/target/debug/hermit-verify"
 
 function hermit_verify {
+    # Github Actions VMs don't expose the perf counters we need to use RCBs:
     "$hverify" --hermit-bin="$hermit" run --isolate-workdir \
-               --hermit-arg="--base-env=empty" --hermit-arg="--env=HERMIT_MODE=strict" --hermit-arg="--preemption-timeout=80000000" \
+               --hermit-arg="--base-env=empty" --hermit-arg="--env=HERMIT_MODE=strict" \
+               --hermit-arg="--no-rcb-time" --hermit-arg="--preemption-timeout=disabled" \
                "$1"
 }
 
-for script in "$rootdir"/tests/shell/*.sh; do
+for script in $(ls "$rootdir"/tests/shell/*.sh); do
     echo; echo "Running shell test: $script"
-#    hermit_verify "$script"
+    hermit_verify "$script"
 done
 
-for bin in $(ls "$rootdir"/target/debug/rustbin_* | grep -v '\.d'); do
-    echo; echo "Running rust test: $bin"
-    hermit_verify "$bin"
+rust_tests=(
+    rustbin_bind_connect_race
+    rustbin_clock_gettime
+    rustbin_futex_timeout
+    rustbin_futex_wait_child
+    rustbin_heap_ptrs
+    rustbin_interrogate_tty
+    rustbin_nanosleep
+    rustbin_network_hello_world
+    rustbin_poll
+    rustbin_print_clock_nanosleep_monotonic_abs_race
+    rustbin_print_clock_nanosleep_monotonic_race
+    rustbin_print_clock_nanosleep_realtime_abs_race
+    rustbin_print_nanosleep_race
+    rustbin_rdtsc
+    rustbin_sched_yield
+    rustbin_socketpair
+    rustbin_stack_ptr
+    rustbin_thread_random
+)
+# Some tests don't work without RCB-timed based preemptions.
+# rustbin_clock_total_order
+# rustbin_exit_group
+# rustbin_futex_and_print
+# rustbin_futex_wake_some
+# rustbin_mem_race
+# rustbin_pipe_basics
+# rustbin_poll_spin
+
+for test in "${rust_tests[@]}";
+do
+    echo; echo "Running rust test: $test"
+    file="$rootdir/target/debug/$test"
+    hermit_verify "$file"
 done
